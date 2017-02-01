ef22d189fb20b2a238d38e78da45222f0a70fd83
Burcu Noyan <burcunoyan@gmail.com>
2017-01-31 23:02:46 -0500

    remove excess comments in html, move bio data to model in app.js

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// refactored version of resumebuilder.js
+
+var model = {
+  // bio data
+  var bio = {
+      "name": "Burcu Noyan",
+      "role": "Front-End Web Developer & Digital Producer",
+      "contacts": {
+        "mobile": "(347) 444-4444",
+        "email": "burcunoyan@gmail.com",
+        "github": "burieberry",
+        "twitter": "@burieberry",
+        "linkedin": "burcunoyan",
+        "location": "New York City"
+      },
+      "biopic": "images/biopic.png",
+      "welcomeMessage": "Welcome!",
+      "skills": ['HTML', 'CSS', 'Javascript', 'CMS', 'Adobe Creative Suite']
+  };
+
+
+
+}
+
+var octopus = {
+  init: function() {
+
+  }
+
+}
+
+var resumeView = {
+
+}
+
